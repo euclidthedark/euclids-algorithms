77c984224f150e57029213488a7abbb25dcb24c6
euclidthedark <euclidthedark@gmail.com>
2020-02-19 22:31:54 -0500

    Create insert method.

--- test/utils/unit/bst.test.js
+++ test/utils/unit/bst.test.js
@@ -46,4 +46,49 @@ describe('./lib/bst', function () {
       itBehavesLikeItHasNullRightNode(bst);
     });
   });
+
+  describe.only('.insert', function () {
+    context('when typeof value !== typeof `this.currentValue`', function () {
+      const bst = new BST(value);
+
+      it('throws TypeError', function () {
+        expect(() => bst.insert('asdf')).to.throw('Tree must contain values of the same type.');
+      });
+    });
+    context('when typeof value === typeof `this.currentValue`', function () {
+      context('and value is not unique', function () {
+        const bst = new BST(value);
+        bst.insert(value);
+
+        it('returns base node with no children', function () {
+          expect(bst.leftNode).to.be.null;
+          expect(bst.rightNode).to.be.null;
+        });
+      });
+      context('and value is unique', function () {
+        it('builds tree 1', function () {
+          const bst = new BST(value);
+
+          bst.insert(2);
+          bst.insert(4);
+          bst.insert(23);
+          bst.insert(27);
+          bst.insert(31);
+          bst.insert(7);
+          bst.insert(11);
+
+          expect(bst.currentValue).to.be.equal(20);
+          // left node assertions
+          expect(bst.leftNode.currentValue).to.be.equal(2);
+          expect(bst.leftNode.rightNode.currentValue).to.be.equal(4);
+          expect(bst.leftNode.rightNode.rightNode.currentValue).to.be.equal(7);
+          expect(bst.leftNode.rightNode.rightNode.rightNode.currentValue).to.be.equal(11);
+          // right node assertions
+          expect(bst.rightNode.currentValue).to.be.equal(23);
+          expect(bst.rightNode.rightNode.currentValue).to.be.equal(27);
+          expect(bst.rightNode.rightNode.rightNode.currentValue).to.be.equal(31);
+        });
+      });
+    });
+  });
 });
